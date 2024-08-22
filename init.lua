88168f87dd585e4e49d9649df1531cd4bc1bfeb8
liopmki <liuyiy4ng@outlook.com>
2024-08-22 13:57:27 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -573,7 +573,44 @@ require('lazy').setup({
         clangd = {}, -- c/cpp
         html = {}, -- html
         cssls = {}, -- css
-        vtsls = {}, -- javascript
+
+        vtsls = { --typescript
+          filetypes = {
+            'javascript',
+            'javascriptreact',
+            'javascript.jsx',
+            'typescript',
+            'typescriptreact',
+            'typescript.tsx',
+          },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = 'always' },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = 'literals' },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+        },
+
         tailwindcss = { -- TailwindCSS
           filetypes_exclude = { 'markdown' },
         },
